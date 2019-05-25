1a86049ee38d6f505b8873d3eec0df5b7d576759
Tina <tinaelha@gmail.com>
2019-05-25 20:38:24 +0200

    Header fini

--- pages/elements/header.php
+++ pages/elements/header.php
@@ -1,9 +1,9 @@
 <nav>
-    <a href=""><img src="images/logo.png" alt="logo" class="secu_logo"></a>
+    <a href="/home" title="Accueil"><img src="assets/images/logo_vertical/eosecurite.png" alt="Logo EO sécurité"></a>
     <ul>  
-        <li><a href="/home">L'entreprise</a></li>
-        <li><a href="/produits">Nos produits</a></li>
-        <li><a href="/formations">Nos formations</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><a href="/home" title="Accueil">L'entreprise</a></li>
+        <li><a href="/produits" title="Produits EO sécurité">Nos produits</a></li>
+        <li><a href="/formations" title="Formations EO sécurité">Nos formations</a></li>
+        <li><a href="/contact" title="Contact EO sécurité">Contact</a></li>
     </ul>
 </nav>

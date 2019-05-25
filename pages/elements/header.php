de50f9a01b7586f0e09848c38fd8c67faff2995b
Tina <tinaelha@gmail.com>
2019-05-25 19:13:53 +0200

    début nav

---
+++ pages/elements/header.php
@@ -0,0 +1,9 @@
+<nav>
+    <a href=""><img src="images/logo.png" alt="logo" class="secu_logo"></a>
+    <ul>  
+        <li><a href="/home">L'entreprise</a></li>
+        <li><a href="/produits">Nos produits</a></li>
+        <li><a href="/formations">Nos formations</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
+</nav>

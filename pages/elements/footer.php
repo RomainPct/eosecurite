d9f93ddcec6afe1633746c28abc89e7944f9dec5
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-05-26 22:29:50 +0200

    push

--- pages/elements/footer.php
+++ pages/elements/footer.php
@@ -18,4 +18,3 @@
     <div id="footer_map">
          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10410.48734700077!2d4.1120199!3d49.2835625!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x582159c6f6770dc7!2sEO+SECURITE!5e0!3m2!1sfr!2sfr!4v1558898213831!5m2!1sfr!2sfr" width="420" height="140" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
-</div>

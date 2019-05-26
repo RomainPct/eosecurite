a52ccc021c3429921d4b5da00ccd49e5a582c666
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-05-26 22:44:27 +0200

    push joli

--- pages/elements/footer.php
+++ pages/elements/footer.php
@@ -1,6 +1,4 @@
-<div id="footer_container">
-    <div id="footer_subContainer">
-        <div id="footer_subContainerLeft">
+<footer id="footer_container">
             <img src="assets/images/logo/logo_small.png" alt="Logo EO sécurité"> 
             <div id="footer_links">
                 <ul>  
@@ -14,7 +12,6 @@
          
             <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&width=77&layout=box_count&action=like&size=large&show_faces=false&share=true&height=65&appId" width="77" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
          </div>
-    </div>
-    <div id="footer_map">
-         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10410.48734700077!2d4.1120199!3d49.2835625!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x582159c6f6770dc7!2sEO+SECURITE!5e0!3m2!1sfr!2sfr!4v1558898213831!5m2!1sfr!2sfr" width="420" height="140" frameborder="0" style="border:0" allowfullscreen></iframe>
-    </div>
+
+         <iframe id="footer_map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10410.48734700077!2d4.1120199!3d49.2835625!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x582159c6f6770dc7!2sEO+SECURITE!5e0!3m2!1sfr!2sfr!4v1558898213831!5m2!1sfr!2sfr" frameborder="0" style="border:0" allowfullscreen></iframe>
+         </footer>

bace6dbd09e31cd5fdd3faea7b15efc93584fc0a
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-05-26 19:56:19 +0200

    footer start

---
+++ pages/elements/footer.php
@@ -0,0 +1,19 @@
+<div id="footer_container">
+    <div id="footer_subContainer">
+        <div id="footer_subContainerLeft">
+            <img src="assets/images/logo/logo_small.png" alt="Logo EO sécurité"> 
+            <div id="footer_links">
+                <ul>  
+                    <li><a href="/home" title="Accueil">L'entreprise</a></li>
+                    <li><a href="/produits" title="Produits EO sécurité">Nos produits</a></li>
+                 <li><a href="/formations" title="Formations EO sécurité">Nos formations</a></li>
+                 <li><a href="/contact" title="Contact EO sécurité">Contact</a></li>
+              </ul>
+         </div>
+         <div id="footer_facebook">
+
+         </div>
+         <div id="footer_map">
+        </div>
+    </div>
+</div>

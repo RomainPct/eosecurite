16a72b948af640c8a77575eacac554fb013be8a6
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-05-26 22:56:01 +0200

    push sauvage

--- pages/elements/footer.php
+++ pages/elements/footer.php
@@ -8,10 +8,8 @@
                     <li><a href="/contact" title="Contact EO sécurité">Contact</a></li>
               </ul>
          </div>
-         <div id="footer_facebook">
-         
-            <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&width=77&layout=box_count&action=like&size=large&show_faces=false&share=true&height=65&appId" width="77" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-         </div>
+
+         <iframe id="footer_facebook" src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2Fcategory%2FLocal-Business%2FEo-s%25C3%25A9curit%25C3%25A9-646702842123804%2F&width=77&layout=box_count&action=like&size=large&show_faces=true&share=true&height=65&appId" width="77" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 
          <iframe id="footer_map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10410.48734700077!2d4.1120199!3d49.2835625!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x582159c6f6770dc7!2sEO+SECURITE!5e0!3m2!1sfr!2sfr!4v1558898213831!5m2!1sfr!2sfr" frameborder="0" style="border:0" allowfullscreen></iframe>
          </footer>

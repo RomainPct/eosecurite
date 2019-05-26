7eb4378c083c4ca603d6c7133ee2b84146a2401e
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-05-26 21:19:43 +0200

    footer + map

--- pages/elements/footer.php
+++ pages/elements/footer.php
@@ -6,14 +6,15 @@
                 <ul>  
                     <li><a href="/home" title="Accueil">L'entreprise</a></li>
                     <li><a href="/produits" title="Produits EO sécurité">Nos produits</a></li>
-                 <li><a href="/formations" title="Formations EO sécurité">Nos formations</a></li>
-                 <li><a href="/contact" title="Contact EO sécurité">Contact</a></li>
+                    <li><a href="/formations" title="Formations EO sécurité">Nos formations</a></li>
+                    <li><a href="/contact" title="Contact EO sécurité">Contact</a></li>
               </ul>
          </div>
          <div id="footer_facebook">
          <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&width=60&layout=box_count&action=like&size=large&show_faces=true&share=true&height=65&appId" width="60" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
          </div>
          <div id="footer_map">
+         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10410.48734700077!2d4.1120199!3d49.2835625!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x582159c6f6770dc7!2sEO+SECURITE!5e0!3m2!1sfr!2sfr!4v1558898213831!5m2!1sfr!2sfr" width="300" height="120" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
     </div>
 </div>

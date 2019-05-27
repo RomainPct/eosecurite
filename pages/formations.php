b29f0c049650bf8c8ea1b095c791ffd8d1371ed7
Tina <tinaelha@gmail.com>
2019-05-27 23:49:04 +0200

    initialisation de la page formations et début HTML

---
+++ pages/formations.php
@@ -0,0 +1,32 @@
+<div class="container_trainings">
+    <div class="trainings">
+        <h2>Nos formations</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto est cumque, molestiae sunt, quae aliquid ad nisi maxime tempora incidunt voluptate ipsa ullam quam provident, debitis vel numquam possimus obcaecati.</p>
+    </div>
+    <div class="characteristics">
+        <p>Les spécificités EO Sécurité</p>
+        <p>Stages accélérés (2x plus rapide)</p>
+        <p>Non-reconnu par xxx</p>
+        <p>Stages de qualité réalisés par des experts</p>
+    </div>
+</div>
+<div class="container_cards">
+    <div>
+        <!--Mettre image ici-->
+        <p>Formation 1</p>
+        <p>Description rapide de la formation en quelques lignes. But, contenu, avantages et intérêts de la formation</p>
+        <!--Bouton "En savoir plus"-->
+    </div>
+    <div>
+        <!--Mettre image ici-->
+        <p>Formation 2</p>
+        <p>Description rapide de la formation en quelques lignes. But, contenu, avantages et intérêts de la formation</p>
+        <!--Bouton "En savoir plus"-->
+    </div>
+    <div>
+        <!--Mettre image ici-->
+        <p>Formation 3</p>
+        <p>Description rapide de la formation en quelques lignes. But, contenu, avantages et intérêts de la formation</p>
+        <!--Bouton "En savoir plus"-->
+    </div>
+</div>

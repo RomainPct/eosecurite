ed1ae28c222a20eee886f46430627ecc9b10e845
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-05-28 18:59:33 +0200

    start home page + change _library h1 h2 h3

--- pages/home.php
+++ pages/home.php
@@ -1,10 +1,13 @@
-<div>
+<main class="containerHome">
 
-    <div class="big__picture">
-        <img src="#" alt="L'entreprise EO Sécurtié">
+    <div class="containerHome__line">
+        <div class="containerHome__line__bigPicture">
+            <img src="#" alt="L'entreprise EO Sécurtié">
+        </div>
+        <div class="containerHome__line__text">
+            <h2>L'entreprise EO Sécurité</h2>
+            <p class="primaryText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil eaque magni minima alias, non nesciunt quae porro libero? Aliquid repellat libero maiores soluta, rem odit quasi. Accusamus, nemo repellat.</p>
+        </div>
     </div>
-    <div>
-        <h2>L'entreprise EO Sécurité</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil eaque magni minima alias, non nesciunt quae porro libero? Aliquid repellat libero maiores soluta, rem odit quasi. Accusamus, nemo repellat.</p>
-    </div>
-</div>
+
+</main>

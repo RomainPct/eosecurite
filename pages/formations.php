8757f3fb817cbcb33107c356de1abb33902cb35b
Tina <tinaelha@gmail.com>
2019-05-28 13:53:11 +0200

    rajout d'image, de div et début scss pour la page formations

--- pages/formations.php
+++ pages/formations.php
@@ -4,28 +4,30 @@
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto est cumque, molestiae sunt, quae aliquid ad nisi maxime tempora incidunt voluptate ipsa ullam quam provident, debitis vel numquam possimus obcaecati.</p>
     </div>
     <div class="characteristics">
-        <p>Les spécificités EO Sécurité</p>
+        <h1>Les spécificités EO Sécurité</h1>
         <p>Stages accélérés (2x plus rapide)</p>
         <p>Non-reconnu par xxx</p>
         <p>Stages de qualité réalisés par des experts</p>
     </div>
 </div>
+<div class="wave_img">
+</div>
 <div class="container_cards">
-    <div>
+    <div class= "cards_training">
         <!--Mettre image ici-->
-        <p>Formation 1</p>
+        <h1>Formation 1</h1>
         <p>Description rapide de la formation en quelques lignes. But, contenu, avantages et intérêts de la formation</p>
         <!--Bouton "En savoir plus"-->
     </div>
-    <div>
+    <div class= "cards_training">
         <!--Mettre image ici-->
-        <p>Formation 2</p>
+        <h1>Formation 2</h1>
         <p>Description rapide de la formation en quelques lignes. But, contenu, avantages et intérêts de la formation</p>
         <!--Bouton "En savoir plus"-->
     </div>
-    <div>
+    <div class= "cards_training">
         <!--Mettre image ici-->
-        <p>Formation 3</p>
+        <h1>Formation 3</h1>
         <p>Description rapide de la formation en quelques lignes. But, contenu, avantages et intérêts de la formation</p>
         <!--Bouton "En savoir plus"-->
     </div>

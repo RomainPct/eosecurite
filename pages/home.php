21b7dbdad10b2f8f60de09ca62c8d6acfd90ed40
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-05-28 18:25:08 +0200

    add _home.scss

---
+++ pages/home.php
@@ -0,0 +1,10 @@
+<div>
+
+    <div class="big__picture">
+        <img src="#" alt="L'entreprise EO Sécurtié">
+    </div>
+    <div>
+        <h2>L'entreprise EO Sécurité</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil eaque magni minima alias, non nesciunt quae porro libero? Aliquid repellat libero maiores soluta, rem odit quasi. Accusamus, nemo repellat.</p>
+    </div>
+</div>

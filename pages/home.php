616f00518a37114adbcbcab5266b7968043264b0
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-05-28 19:27:38 +0200

    add 2 lines of 3 in the home page

--- pages/home.php
+++ pages/home.php
@@ -1,12 +1,20 @@
 <main class="containerHome">
-
     <div class="containerHome__line">
         <div class="containerHome__line__bigPicture">
             <img src="#" alt="L'entreprise EO Sécurtié">
         </div>
-        <div class="containerHome__line__text">
+        <div class="containerHome__line__textJustifyLeft">
             <h2>L'entreprise EO Sécurité</h2>
             <p class="primaryText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil eaque magni minima alias, non nesciunt quae porro libero? Aliquid repellat libero maiores soluta, rem odit quasi. Accusamus, nemo repellat.</p>
+        </div>
+    </div>
+    <div class="containerHome__line">
+        <div class="containerHome__line__textJustifyRight">
+            <h2>Nos spécificités</h2>
+            <p class="primaryText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil eaque magni minima alias, non nesciunt quae porro libero? Aliquid repellat libero maiores soluta, rem odit quasi. Accusamus, nemo repellat.</p>
+        </div>
+        <div class="containerHome__line__bigPicture">
+            <img src="#" alt="L'entreprise EO Sécurtié">
         </div>
     </div>
 

f5e2103fad697a0ea805e0df6fb74107c71cc1b7
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-05-28 20:42:20 +0200

    add lastLine in the home page but got problem with the primaryButton

--- pages/home.php
+++ pages/home.php
@@ -5,28 +5,34 @@
         </div>
         <div class="containerHome__line__textJustifyLeft">
             <h2>L'entreprise EO Sécurité</h2>
-            <p class="primaryText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil eaque magni minima alias, non nesciunt quae porro libero? Aliquid repellat libero maiores soluta, rem odit quasi. Accusamus, nemo repellat.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil eaque magni minima alias, non nesciunt quae porro libero? Aliquid repellat libero maiores soluta, rem odit quasi. Accusamus, nemo repellat.</p>
         </div>
     </div>
     <div class="containerHome__line">
         <div class="containerHome__line__textJustifyRight">
             <h2>Nos spécificités</h2>
-            <p class="primaryText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil eaque magni minima alias, non nesciunt quae porro libero? Aliquid repellat libero maiores soluta, rem odit quasi. Accusamus, nemo repellat.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil eaque magni minima alias, non nesciunt quae porro libero? Aliquid repellat libero maiores soluta, rem odit quasi. Accusamus, nemo repellat.</p>
         </div>
         <div class="containerHome__line__bigPicture">
-            <img src="#" alt="L'entreprise EO Sécurtié">
+            <img src="#" alt="Nos spécificités">
         </div>
     </div>
-
     <div class="containerHome__lastLine">
         <div class="containerHome__lastLine__bloc">
-            <h3>Nos produits</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore repellat quos magni quo quod natus porro sequi, neque voluptatibus illo assumenda voluptate. Non accusantium sint sed maxime quibusdam quaerat hic?</p>
+            <div class="containerHome__lastLine__bloc__smallPicture">
+                <img src="#" alt="Nos produits">
+            </div>
+            <h3 class="white">Nos produits</h3>
+            <p class="white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore repellat quos magni quo quod natus porro sequi, neque voluptatibus illo assumenda voluptate. Non accusantium sint sed maxime quibusdam quaerat hic?</p>
+            <a clas="primaryButton white" href="#">Découvrir nos produits</a>
         </div>
         <div class="containerHome__lastLine__bloc">
-        <h3>Nos formations</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore repellat quos magni quo quod natus porro sequi, neque voluptatibus illo assumenda voluptate. Non accusantium sint sed maxime quibusdam quaerat hic?</p>
+            <div class="containerHome__lastLine__bloc__smallPicture">
+                <img src="#" alt="Nos formations">
+            </div>
+            <h3 class="white">Nos formations</h3>
+            <p class="white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore repellat quos magni quo quod natus porro sequi, neque voluptatibus illo assumenda voluptate. Non accusantium sint sed maxime quibusdam quaerat hic?</p>
+            <a clas="primaryButton white" href="#">Découvrir nos formations</a>
         </div>
     </div>
-
 </main>

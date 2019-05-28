a38f6314472c9a7de5c67968c86fa94f64f93415
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-05-28 18:50:48 +0200

    push partners 1

---
+++ pages/partenaires.php
@@ -0,0 +1,44 @@
+<main id="part__container">
+    <section id="part__displayContainer">
+        <div id="part__leftDisplay">
+            <h2>Nos Produits</h2>
+            <p>Texte qualité des produits et des partenaires + avantages de travailler avec EO Sécurité pour ce type de produits .Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur</p>
+        </div>
+        <img id="part__rightDisplay">
+    </section>
+    <section id="part__slider">
+        <div class="part__cards">
+            <img>
+            <h4>Eratum</h4>
+        </div>
+        <div class="part__cards">
+            <img>
+            <h4>Eratum</h4>
+        </div>
+        <div class="part__cards">
+            <img>
+            <h4>Eratum</h4>
+        </div>
+        <div class="part__cards">
+            <img>
+            <h4>Eratum</h4>
+        </div>
+        <div class="part__cards">
+            <img>
+            <h4>Eratum</h4>
+        </div>
+        <div class="part__cards">
+            <img>
+            <h4>Eratum</h4>
+        </div>
+        <div class="part__cards">
+            <img>
+            <h4>Eratum</h4>
+        </div>
+        <div class="part__cards">
+            <img>
+            <h4>Eratum</h4>
+        </div>
+    </section>
+    <section id="part__partners"></section>
+</main>

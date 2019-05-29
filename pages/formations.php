8fbecf53ba9639abcafb805f9b910f54d0ccdf1a
Tina <tinaelha@gmail.com>
2019-05-30 00:04:17 +0200

    ajustement h2, container cards positionnement et translate pour la page formations

--- pages/formations.php
+++ pages/formations.php
@@ -5,7 +5,7 @@
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto est cumque, molestiae sunt, quae aliquid ad nisi maxime tempora incidunt voluptate ipsa ullam quam provident, debitis vel numquam possimus obcaecati.</p>
         </div>
         <div class="containerFormations__containerTrainings__trainings__characteristics">
-            <h1 class="white">Les spécificités EO Sécurité</h1>
+            <h2 class="white">Les spécificités EO Sécurité</h2>
             <p class="white">Stages accélérés (2x plus rapide)</p>
             <p class= "white">Non-reconnu par xxx</p>
             <p class="white">Stages de qualité réalisés par des experts</p>
@@ -14,20 +14,20 @@
 
     <div class="containerCards">
         <div class= "containerCards__cardsTraining">
-            <!--Mettre image ici-->
-            <h1 class="white">Formation 1</h1>
+            <div class="containerCards__cardsTraining__img"></div>
+            <h2 class="white">Formation 1</h2>
             <p class="white">Description rapide de la formation en quelques lignes. But, contenu, avantages et intérêts de la formation</p>
             <!--Bouton "En savoir plus"-->
         </div>
         <div class= "containerCards__cardsTraining">
-            <!--Mettre image ici-->
-            <h1 class="white">Formation 2</h1>
+            <div class="containerCards__cardsTraining__img"></div>
+            <h2 class="white">Formation 2</h2>
             <p class="white">Description rapide de la formation en quelques lignes. But, contenu, avantages et intérêts de la formation</p>
             <!--Bouton "En savoir plus"-->
         </div>
         <div class= "containerCards__cardsTraining">
-            <!--Mettre image ici-->
-            <h1 class="white">Formation 3</h1>
+            <div class="containerCards__cardsTraining__img"></div>
+            <h2 class="white">Formation 3</h2>
             <p class="white">Description rapide de la formation en quelques lignes. But, contenu, avantages et intérêts de la formation</p>
             <!--Bouton "En savoir plus"-->
         </div>

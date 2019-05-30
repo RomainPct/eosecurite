504640929e5d09fdaa82350018c3ef74645c6b42
Hugo Leroy <hugo.leroy15@gmail.com>
2019-05-30 17:07:18 +0200

    variable scss text color white

--- pages/contact.php
+++ pages/contact.php
@@ -1,6 +1,13 @@
 <main class="contactContainer">
     <section class="contactContainer__section">
         <div class="contactContainer__section__leftdiv">
+        <iframe class="contactContainer__section__leftdiv__map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10410.48734700077!2d4.1120199!3d49.2835625!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x582159c6f6770dc7!2sEO+SECURITE!5e0!3m2!1sfr!2sfr!4v1558898213831!5m2!1sfr!2sfr" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <h3>EO Sécurité</h3>
+            <p class="secondaryTextWhite">Adresse :</p>
+            <p class="secondaryTextWhite">x rue tagada</p>
+            <p class="secondaryTextWhite">94160 Reims</p>
+            <p class="secondaryTextWhite">Téléphone : XX XX XX XX XX</p>
+            <p class="secondaryTextWhite">Adresse mail : adresse@mail.com</p>
         </div>
         <div class="contactContainer__section__rightdiv">
         </div>

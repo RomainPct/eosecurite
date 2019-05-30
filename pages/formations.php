c56e70816cf98d46019291df05326437cf968e6b
Tina <tinaelha@gmail.com>
2019-05-30 11:33:01 +0200

    intégration "button cards", modifications padding et réglage menu scss page formations

--- pages/formations.php
+++ pages/formations.php
@@ -5,7 +5,7 @@
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto est cumque, molestiae sunt, quae aliquid ad nisi maxime tempora incidunt voluptate ipsa ullam quam provident, debitis vel numquam possimus obcaecati.</p>
         </div>
         <div class="containerFormations__containerTrainings__trainings__characteristics">
-            <h2 class="white">Les spécificités EO Sécurité</h2>
+            <h3 class="white">Les spécificités EO Sécurité</h3>
             <p class="white">Stages accélérés (2x plus rapide)</p>
             <p class= "white">Non-reconnu par xxx</p>
             <p class="white">Stages de qualité réalisés par des experts</p>
@@ -16,26 +16,32 @@
         <div class= "containerCards__cardsTraining">
             <div class="containerCards__cardsTraining__imgFormations"></div>
             <div class="containerCards__cardsTraining__contentFormations">
-                <h2 class="white">Formation 1</h2>
+                <h3 class="white">Formation 1</h3>
                 <p class="white">Description rapide de la formation en quelques lignes. But, contenu, avantages et intérêts de la formation</p>
             </div>
-            <!--Bouton "En savoir plus"-->
+            <div class="containerCards_cardsTraining_buttonFormations">
+                <a class="primaryButton" href="../formation" type="button" title="Bouton formations">En savoir plus</a>
+            </div>
         </div>
         <div class= "containerCards__cardsTraining">
             <div class="containerCards__cardsTraining__imgFormations"></div>
             <div class="containerCards__cardsTraining__contentFormations">
-                <h2 class="white">Formation 2</h2>
+                <h3 class="white">Formation 2</h3>
                 <p class="white">Description rapide de la formation en quelques lignes. But, contenu, avantages et intérêts de la formation</p>
             </div>
-            <!--Bouton "En savoir plus"-->
+            <div class="containerCards_cardsTraining_buttonFormations">
+                <a class="primaryButton" href="../formation" type="button" title="Bouton formations">En savoir plus</a>
+            </div>
         </div>
         <div class= "containerCards__cardsTraining">
             <div class="containerCards__cardsTraining__imgFormations"></div>
             <div class="containerCards__cardsTraining__contentFormations">
-                <h2 class="white">Formation 3</h2>
+                <h3 class="white">Formation 3</h3>
                 <p class="white">Description rapide de la formation en quelques lignes. But, contenu, avantages et intérêts de la formation</p>
             </div>
-            <!--Bouton "En savoir plus"-->
+            <div class="containerCards_cardsTraining_buttonFormations">
+                <a class="primaryButton white" href="../formation" type="button" title="Bouton formations">En savoir plus</a>
+            </div>
         </div>
     </div>
 </main>

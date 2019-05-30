a80dbfce9928c45f2bed4cc2ef9c59f4408e7e44
Hugo Leroy <hugo.leroy15@gmail.com>
2019-05-30 14:28:06 +0200

    init contact v2
    Potentiel erreur github je test un truc

---
+++ pages/contact.php
@@ -0,0 +1,8 @@
+<main class="contactContainer">
+    <section class="contactContainer__section">
+        <div class="contactContainer__section__leftdiv">
+        </div>
+        <div class="contactContainer__section__rightdiv">
+        </div>
+    </section>
+</main>

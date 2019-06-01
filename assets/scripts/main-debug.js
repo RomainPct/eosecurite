317179118c97ffc830f51069cdc9a12458f7a1eb
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-06-01 15:16:28 +0200

    push produits

--- assets/scripts/main-debug.js
+++ assets/scripts/main-debug.js
@@ -3,16 +3,14 @@
 Global
 -----------
 */
-var part__cntWd, part__sldWd, part__tb
+let part__sliderLeft = document.querySelector('#part__slider').style.marginLeft + 'vw'
 
-function part__mouseMove() {
-    part__cntWd = document.querySelector('#part__partners').innerWidth;
-		part__tb = document.querySelector('.part__partnersSquare');
-		part__sldWd = part__tb.outerWidth;
-		
-		document.querySelector('part__partners').mousemove(function(e){
-			part__tb.css({left: ((part__cntWd - part__sldWd)*((e.pageX / part__cntWd).toFixed(3))).toFixed(1) +"px" });
- 		});
+document.querySelector('#part__slider').addEventListener("mousemove", roll)
+function roll(){
+let part__sliderLeft = document.querySelector('#part__slider').style.marginLeft -= 2
 }
+setTimeout(function roll(){
+; }, 30);
 
-part__mouseMove()
+
+console.log(part__sliderLeft)
--- pages/produits.php
+++ pages/produits.php
@@ -1,8 +1,10 @@
 <main id="part__container">
     <section id="part__displayContainer">
-        <div id="part__leftDisplay">
-            <h2>Nos Produits</h2>
-            <p>Texte qualité des produits et des partenaires + avantages de travailler avec EO Sécurité pour ce type de produits .Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur</p>
+        <div id="part__leftCont">
+            <div id="part__leftDisplay">
+              <h2>Nos Produits</h2>
+             <p>Texte qualité des produits et des partenaires + avantages de travailler avec EO Sécurité pour ce type de produits .Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur</p>
+            </div>
         </div>
         <img src="https://picsum.photos/600/300" id="part__rightDisplay">
     </section>
@@ -41,18 +43,21 @@
         </div>
     </section>
     <section id="part__partners">
-        <div class="part__partnersSquare"></div>
-        <div class="part__partnersSquare"></div>
-        <div class="part__partnersSquare"></div>
-        <div class="part__partnersSquare"></div>
-        <div class="part__partnersSquare"></div>
-        <div class="part__partnersSquare"></div>
-        <div class="part__partnersSquare"></div>
-        <div class="part__partnersSquare"></div>
-        <div class="part__partnersSquare"></div>
-        <div class="part__partnersSquare"></div>
-        <div class="part__partnersSquare"></div>
-        <div class="part__partnersSquare"></div>
+        <h2>Nos paretnaires</h2>
+        <div id="part__squareCont">
+            <div class="part__partnersSquare"></div>
+            <div class="part__partnersSquare"></div>
+            <div class="part__partnersSquare"></div>
+            <div class="part__partnersSquare"></div>
+            <div class="part__partnersSquare"></div>
+            <div class="part__partnersSquare"></div>
+            <div class="part__partnersSquare"></div>
+            <div class="part__partnersSquare"></div>
+            <div class="part__partnersSquare"></div>
+            <div class="part__partnersSquare"></div>
+            <div class="part__partnersSquare"></div>
+            <div class="part__partnersSquare"></div>
+        </div>
     </section>
     <section id="part__contact">
         <a href="/contact" title="Contact EO sécurité" class="primaryButton">Prenez contact avec EO Sécurité</a>

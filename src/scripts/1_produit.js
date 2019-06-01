45f6d8a8b6c92df9034d0901ef06695b2b0b7fb0
Romain Penchenat <romain.penchenat@gmail.com>
2019-06-01 13:04:52 +0200

    Nettoyage

--- src/scripts/1_produit.js
+++ src/scripts/1_produit.js
@@ -5,8 +5,7 @@ function part__mouseMove() {
 		part__tb = document.querySelector('.part__partnersSquare');
 		part__sldWd = part__tb.outerWidth;
 		
-		document.querySelector('part__partners').mousemove(function(e)
-		{
+		document.querySelector('part__partners').mousemove(function(e){
 			part__tb.css({left: ((part__cntWd - part__sldWd)*((e.pageX / part__cntWd).toFixed(3))).toFixed(1) +"px" });
  		});
 }

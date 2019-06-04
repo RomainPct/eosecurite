c6d2c176e7f768043cd7f4cb67f74eeb90e52717
Tina <tinaelha@gmail.com>
2019-06-04 19:10:22 +0200

    page formation détaillée finie

--- pages/formationdetaillee.php
+++ pages/formationdetaillee.php
@@ -10,14 +10,14 @@
         </div>
     </div>
     <div class="containerFormationDetaillee__formationAskQuotation">
-        <div class="containerFormationDetaillee__formationAskQuotation__informationFormationQuotation">
+        <div class="containerFormationDetaillee__formationAskQuotation__informationQuotation">
             <p>Principe de la formation en 2/3 lignes… Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.</p>
-            <p>Public: Toutes personnes de l'entreprise.</p>
-            <p>Durée: 2 journées (14 heures)</p>
-            <p>Lieu: Dans les locaux de l'entreprise (les salariés apporteront leurs E.P.I)</p>
-        </div>
-        <div class="containerFormationDetaillee_formationAskQuotation_buttonQuotation">
-            <a class="primaryButton white" href="../contact" title="Bouton formation detaillee">Demander un devis</a>
+            <p><strong>Public:</strong> Toutes personnes de l'entreprise.</p>
+            <p><strong>Durée:</strong> 2 journées (14 heures)</p>
+            <p><strong>Lieu:</strong> Dans les locaux de l'entreprise (les salariés apporteront leurs E.P.I)</p>
+            <div class="containerFormationDetaillee__formationAskQuotation__informationQuotation__buttonQuotation">
+                <a class="primaryButton white" href="../contact" title="Bouton formation detaillee">Demander un devis</a>
+            </div>
         </div>
     </div>
 </main>

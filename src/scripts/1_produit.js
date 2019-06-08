fbd946ecb223741c0f8661137eda02cf1ad22ad4
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-06-08 19:26:13 +0200

    prod
    j

--- src/scripts/1_produit.js
+++ src/scripts/1_produit.js
@@ -2,23 +2,22 @@
 var slider = document.querySelector('#part__slider')
 if (slider != null) {
     var sliderWidth = slider.offsetWidth
-console.log(sliderWidth)
+
 // récupérer la width de l'écran de l'utilisateur
 var screenWidth = window.innerWidth - 220
-console.log(screenWidth)
+
 // Determiner la taille en vw de #part_slider
 var prctSlider = (sliderWidth / screenWidth) * 100
-console.log(prctSlider)
+
 // Déterminer la taille qui dépasse en vw
 var offScreen = prctSlider - 100
-console.log(offScreen)
+
 
 
 // Detecter le mouvement de la souris sur la div #part__sliderCont
     slider.onmousemove = function(e){
         let rapportWidth = (e.clientX / window.innerWidth) * offScreen
         slider.style.left = -(rapportWidth)+ "vw"
-        console.log(rapportWidth)
     }
     // selon la pos x de ta souris
     // Calculer sa position en pourcentage de la width de l'écran

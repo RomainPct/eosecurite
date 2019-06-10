f9ac7873af136d415f885b6a0de4d26a35d424a4
Tina <tinaelha@gmail.com>
2019-06-10 18:41:55 +0200

    supression candide formation detaillee

--- pages/formationdetaillee.php
+++ pages/formationdetaillee.php
@@ -1,12 +1,9 @@
-<?php 
-$c = new CandidePage ('formation_detaillee');
-?>
 <h1>Les informations des formations de EO Sécurité Reims</h1>
 <main class= "containerFormationDetaillee">
     <div class="containerFormationDetaillee__contentFormationDetaillee">
         <div class="containerFormationDetaillee__contentFormationDetaillee__titleFormationDetaillee">
-            <h2><?php $c->text('titre_de_la_formation'); ?></h2>
-            <p><?php $c->text('contexte_de_la_formation'); ?></p>
+            <h2>Titre de la formation</h2>
+            <p>Contexte de la formation Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aperiam, eum doloremque ipsam quas temporibus quis et vel distinctio mollitia sunt consequuntur cum amet praesentium! Aperiam itaque deleniti possimus nemo?</p>
         </div>
         <div class="containerFormationDetaillee__contenFormationDetaillee__descriptionFormationDetaillee">
                 <!-- Contenu de la formation (modulable avec l'espace de gestion) -->
@@ -14,7 +11,7 @@ $c = new CandidePage ('formation_detaillee');
     </div>
     <div class="containerFormationDetaillee__formationAskQuotation">
         <div class="containerFormationDetaillee__formationAskQuotation__informationQuotation">
-            <p><?php $c->text('principe_de_la_formation_en_quelques_lignes'); ?></p>
+            <p>Principe de la formation en quelques lignes</p>
             <p><strong>Public:</strong> Toutes personnes de l'entreprise.</p>
             <p><strong>Durée:</strong> 2 journées (14 heures)</p>
             <p><strong>Lieu:</strong> Dans les locaux de l'entreprise (les salariés apporteront leurs E.P.I)</p>

6c661be17366fbe6c24968fa60a5a68fe0036e8d
Hugo Leroy <hugo.leroy15@gmail.com>
2019-06-10 15:28:36 +0200

    Js contact update

--- src/scripts/3_contact.js
+++ src/scripts/3_contact.js
@@ -37,40 +37,41 @@ let form = document.querySelector('form')
 let inputs = document.querySelectorAll('input')
 let check = document.querySelector('.check')
 let textarea = document.querySelector('textarea')
-let error = document.querySelector('.error')
+let error = document.querySelectorAll('.error')
 
 inputs.forEach(function(input){
     input.addEventListener("input", function(e){
         error.innerHTML = "Champs invalide !"
-        error.classList.remove = "error"
+        error.classList.remove("error")
         console.log(error)
     })
 })
 
 textarea.addEventListener("input", function(textarea){
     error.innerHTML = "Champs invalide !"
-    error.classList.remove = "error"
+    error.classList.remove("error")
 })
 
 form.addEventListener("submit", function(e){
-    if(inputs[0].validity.valid) {
+    if(inputs[0].value.lenght <= 0) {
         error.innerHTML = "Champs invalide !"
-        error.classList.add = "error active"
+        error.classList.add("error active")
         e.preventDefault()
     }
     else if(inputs[1].value.lenght <= 0) {
         error.innerHTML = "Champs invalide !"
-        error.classList.add = "error active"
+        error.classList.add("error active")
         e.preventDefault()
     }
     else if(inputs[2].value.lenght <= 0) {
         error.innerHTML = "Champs invalide !"
-        error.classList.add = "error active"
+        error.classList.add("error active")
         e.preventDefault()
     }
     else if(inputs[3].value.lenght <= 0) {
         error.innerHTML = "Champs invalide !"
-        error.classList.add = "error active"
+        error.classList.add("error active")
         e.preventDefault()
+        console.log(e)
     }
-})
+},false)

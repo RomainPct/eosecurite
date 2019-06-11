af2676dab356e6dbd923de41b25dc76ee1bc7c76
Tina <tinaelha@gmail.com>
2019-06-11 23:07:03 +0200

    candide collection item page formation detaillee et supression cards formations

--- pages/formationdetaillee.php
+++ pages/formationdetaillee.php
@@ -1,20 +1,24 @@
+<?php 
+$c = new CandideCollectionItem('formations', $_GET["id"]);
+?>
 <h1>Les informations des formations de EO Sécurité Reims</h1>
 <main class= "containerFormationDetaillee">
     <div class="containerFormationDetaillee__contentFormationDetaillee">
         <div class="containerFormationDetaillee__contentFormationDetaillee__titleFormationDetaillee">
-            <h2>Titre de la formation</h2>
-            <p>Contexte de la formation Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aperiam, eum doloremque ipsam quas temporibus quis et vel distinctio mollitia sunt consequuntur cum amet praesentium! Aperiam itaque deleniti possimus nemo?</p>
+            <h2><?php $c->text('nom_de_la_formation'); ?></h2>
+            <p><?php $c->text('description_de_la_formation'); ?></p>
         </div>
         <div class="containerFormationDetaillee__contenFormationDetaillee__descriptionFormationDetaillee">
                 <!-- Contenu de la formation (modulable avec l'espace de gestion) -->
+                <?php $c->text('descriptif_complet_de_la_formation', true); ?>
         </div>
     </div>
     <div class="containerFormationDetaillee__formationAskQuotation">
         <div class="containerFormationDetaillee__formationAskQuotation__informationQuotation">
-            <p>Principe de la formation en quelques lignes</p>
-            <p><strong>Public:</strong> Toutes personnes de l'entreprise.</p>
-            <p><strong>Durée:</strong> 2 journées (14 heures)</p>
-            <p><strong>Lieu:</strong> Dans les locaux de l'entreprise (les salariés apporteront leurs E.P.I)</p>
+            <p><?php $c->text('principe_de_la_formation'); ?></p>
+            <p><strong>Public:</strong><?php $c->text('public_de_l_entreprise'); ?></p>
+            <p><strong>Durée:</strong><?php $c->text('nombre_de_jours'); ?></p>
+            <p><strong>Lieu:</strong><?php $c->text('lieu_de_la_formation'); ?></p>
             <div class="containerFormationDetaillee__formationAskQuotation__informationQuotation__buttonQuotation">
                 <a class="primaryButton white" href="../contact" title="Bouton formation detaillee">Demander un devis</a>
             </div>
--- pages/formations.php
+++ pages/formations.php
@@ -1,6 +1,6 @@
 <?php 
-$c = new CandidePage ('formations');
-$c1 = new CandideCollection ('formations');
+$c = new CandidePage('formations');
+$c1 = new CandideCollection('formations');
 ?>
 <h1>Les formations de EO Sécurité Reims</h1>
 <main class="containerFormations">
@@ -23,11 +23,11 @@ $c1 = new CandideCollection ('formations');
         ?>
         <div class= "containerCardsFormations__cardsTraining">
             <div class="containerCardsFormations__cardsTraining__imgFormations">
-                <img src="<?php $c1->image('image_de_la_formation_numero_x', $id,[400,200]) ?>" alt="">
+                <img src="<?php $c1->image('image_de_la_formation', $id,[400,200]) ?>" alt="">
             </div>
             <div class="containerCardsFormations__cardsTraining__contentFormations">
-                <h3 class="white"><?php $c1->text('nom_de_la_formation_numero_x', $id); ?></h3>
-                <p class="white"><?php $c1->text('description_de_la_formation_numero_x', $id); ?></p>
+                <h3 class="white"><?php $c1->text('nom_de_la_formation', $id); ?></h3>
+                <p class="white"><?php $c1->text('description_de_la_formation', $id); ?></p>
             </div>
             <div class="containerCardsFormations__cardsTraining__buttonFormations">
                 <a class="primaryButton white" href="formation/<?php echo $id ?>" title="<?php $c1->text('nom_de_la_formation', $id) ?> EO Sécurité">En savoir plus</a>
@@ -35,43 +35,6 @@ $c1 = new CandideCollection ('formations');
         </div>
         <?php
     }
-    ?>
-
-        <div class= "containerCardsFormations__cardsTraining">
-            <div class="containerCardsFormations__cardsTraining__imgFormations">
-                <img src="<?php $c->image('image_de_la_formation_numero_1',[400,200]) ?>" alt="">
-            </div>
-            <div class="containerCardsFormations__cardsTraining__contentFormations">
-                <h3 class="white"><?php $c->text('nom_de_la_formation_numero_1'); ?></h3>
-                <p class="white"><?php $c->text('description_de_la_formation_numero_1'); ?></p>
-            </div>
-            <div class="containerCardsFormations__cardsTraining__buttonFormations">
-                <a class="primaryButton white" href="formation/0" title="Bouton formations">En savoir plus</a>
-            </div>
-        </div>
-        <div class= "containerCardsFormations__cardsTraining">
-            <div class="containerCardsFormations__cardsTraining__imgFormations">
-                <img src="<?php $c->image('image_de_la_formation_numero_2',[400,200]) ?>" alt="">
-            </div>
-            <div class="containerCardsFormations__cardsTraining__contentFormations">
-                <h3 class="white"><?php $c->text('nom_de_la_formation_numero_2'); ?></h3>
-                <p class="white"><?php $c->text('description_de_la_formation_numero_2'); ?></p>
-            </div>
-            <div class="containerCardsFormations__cardsTraining__buttonFormations">
-                <a class="primaryButton white" href="formation/0" title="Bouton formations">En savoir plus</a>
-            </div>
-        </div>
-        <div class= "containerCardsFormations__cardsTraining">
-            <div class="containerCardsFormations__cardsTraining__imgFormations">
-                <img src="<?php $c->image('image_de_la_formation_numero_3',[400,200]) ?>" alt="">
-            </div>
-            <div class="containerCardsFormations__cardsTraining__contentFormations">
-                <h3 class="white"><?php $c->text('nom_de_la_formation_numero_3'); ?></h3>
-                <p class="white"><?php $c->text('description_de_la_formation_numero_3'); ?></p>
-            </div>
-            <div class="containerCardsFormations__cardsTraining__buttonFormations">
-                <a class="primaryButton white" href="formation/0" title="Bouton formations">En savoir plus</a>
-            </div>
-        </div>
+    ?> 
     </div>
 </main>

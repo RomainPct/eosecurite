c2b1b255dd225080d8f7e03487e2767d9787c16a
Hugo Leroy <hugo.leroy15@gmail.com>
2019-06-14 11:23:05 +0200

    init services

---
+++ pages/services.php
@@ -0,0 +1,32 @@
+<h1>services de eosécurité</h1>
+<main class="servicesContainer">
+    <section class="servicesContainer__servicesSection">
+        <div class="servicesContainer__servicesSection__toppage">
+            <h2>Nos services</h2>
+            <p>EO Sécurité vous propose également des services annexes dans le but de simplifier la gestion de votre entreprise via la création de paquetage, la réalisation d’audits mais également la réalisation de documents et la livraison de nos produits.</p>
+        </div>
+        <div class="servicesContainer__servicesSection__cat">
+            <h3 class="white">Personnalisation</h3>
+            <p class="white">- Personnalisation de vêtements / sérigraphie / broderie
+               - Réalisation de paquetage individuels sur mesure
+               - Paquetage standard (même contenue pour chaque salarié, seul les tailles changes) ou paquetage 
+               100% personnalisé (choix des produits, quantités et tailles par salariés selon un budget défini et un 
+               catalogue personnalisé pour votre entreprise)</p>
+        </div>
+        <div class="servicesContainer__servicesSection__cat">
+            <h3 class="white">Contrôles et audits</h3>
+            <p class="white">- Contrôle réglementaire de harnais et matériels travaux en hauteur
+               - Contrôle annuelle obligatoire, vérification des numéros de séries, de l’état général du matériel, 
+               - Contrôle réglementaire des échelles et échafaudages
+               - Audits entreprises ou chantiers</p>
+        </div>
+        <div class="servicesContainer__servicesSection__cat">
+            <h3 class="white">Documents</h3>
+            <p class="white">- Réalisation de documents uniques (création, misle à jour ou aide à la rédaction)</p>
+        </div>
+        <div class="servicesContainer__servicesSection__cat">
+            <h3 class="white">Livraison</h3>
+            <p class="white">Livraison par nos soins (Reims, Epernay, et environs) ou service de livraison via un transporteur</p>
+        </div>
+    </section>
+</main>

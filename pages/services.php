5f37541b33a671e35b042cc65d5159d945e14b89
Hugo Leroy <hugo.leroy15@gmail.com>
2019-06-14 13:57:36 +0200

    html css page services

--- pages/services.php
+++ pages/services.php
@@ -1,12 +1,12 @@
 <h1>services de eosécurité</h1>
 <main class="servicesContainer">
     <section class="servicesContainer__servicesSection">
-        <div class="servicesContainer__servicesSection__toppage">
+        <div class="servicesContainer__servicesSection__cattop">
             <h2>Nos services</h2>
             <p>EO Sécurité vous propose également des services annexes dans le but de simplifier la gestion de votre entreprise via la création de paquetage, la réalisation d’audits mais également la réalisation de documents et la livraison de nos produits.</p>
         </div>
         <div class="servicesContainer__servicesSection__cat">
-            <h3 class="white">Personnalisation</h3>
+            <h2 class="white">Personnalisation</h2>
             <p class="white">- Personnalisation de vêtements / sérigraphie / broderie
                - Réalisation de paquetage individuels sur mesure
                - Paquetage standard (même contenue pour chaque salarié, seul les tailles changes) ou paquetage 
@@ -14,19 +14,22 @@
                catalogue personnalisé pour votre entreprise)</p>
         </div>
         <div class="servicesContainer__servicesSection__cat">
-            <h3 class="white">Contrôles et audits</h3>
+            <h2 class="white">Contrôles et audits</h2>
             <p class="white">- Contrôle réglementaire de harnais et matériels travaux en hauteur
                - Contrôle annuelle obligatoire, vérification des numéros de séries, de l’état général du matériel, 
                - Contrôle réglementaire des échelles et échafaudages
                - Audits entreprises ou chantiers</p>
         </div>
         <div class="servicesContainer__servicesSection__cat">
-            <h3 class="white">Documents</h3>
+            <h2 class="white">Documents</h2>
             <p class="white">- Réalisation de documents uniques (création, misle à jour ou aide à la rédaction)</p>
         </div>
         <div class="servicesContainer__servicesSection__cat">
-            <h3 class="white">Livraison</h3>
+            <h2 class="white">Livraison</h2>
             <p class="white">Livraison par nos soins (Reims, Epernay, et environs) ou service de livraison via un transporteur</p>
+        </div>
+        <div class="primaryButton servicesContainer__servicesSection__button">
+            <button class="primaryButton contactContainer__section__rightdiv__button">Prendre contact avec EO Sécurité</button>
         </div>
     </section>
 </main>

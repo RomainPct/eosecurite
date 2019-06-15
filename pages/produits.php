1238b6b7afc2dd3ad9c5cfe09948b0f7c765d476
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-06-15 15:06:04 +0200

    produits et partenaires

--- pages/produits.php
+++ pages/produits.php
@@ -29,21 +29,28 @@ $c1 = new CandideCollection('produits');
     <section id="part__partners" class="comingRight">
         <h2>Nos partenaires</h2>
         <div id="part__squareCont">
-            <div class="part__partnersSquare"></div>
-            <div class="part__partnersSquare"></div>
-            <div class="part__partnersSquare"></div>
-            <div class="part__partnersSquare"></div>
-            <div class="part__partnersSquare"></div>
-            <div class="part__partnersSquare"></div>
-            </div>
-            <div id="part__squareCont">
-            <div class="part__partnersSquare"></div>
-            <div class="part__partnersSquare"></div>
-            <div class="part__partnersSquare"></div>
-            <div class="part__partnersSquare"></div>
-            <div class="part__partnersSquare"></div>
-            <div class="part__partnersSquare"></div>
-            </div>
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/defibrion.png" alt="Defibrion"></div>
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/aimont.jpg" alt="Aimont"></div>
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/jallatte.jpg" alt="Jallatte"></div>
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/upower.jpg" alt="Upower"></div>
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/guyard.jpg" alt="Guyard"></div>
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/heckel.jpg" alt="Heckel"></div>
+        </div>
+        <div id="part__squareCont">
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/uvex.png" alt="Uvex"></div>
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/honeywell.jpg" alt="Honeywell"></div>
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/maxiflex.png" alt="Maxiflex"></div>
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/msa.png" alt="Msa"></div>
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/dickies.png" alt="Dickies"></div>
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/carhartt.jpg" alt="Carhartt"></div>
+        </div>
+        <div id="part__squareCont">
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/texet.png" alt="Texet"></div>
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/workzone.jpg" alt="Workzone"></div>
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/engel.png" alt="Engel" width="130px"></div>
+            <div class="part__partnersSquare"> <img src="assets/images/partenaires/cepovett.png" alt="Cepovett"></div>
+            <div class="part__partnersSquare"> <img src="" alt=""></div>
+            <div class="part__partnersSquare"> <img src="" alt=""></div>
         </div>
     </section>
     <section id="part__contact">

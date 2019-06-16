42d24e3d0afcd652e7e70078e527df55ea90f9c6
Tina <tinaelha@gmail.com>
2019-06-16 19:50:24 +0200

    gestion candide page services

--- pages/services.php
+++ pages/services.php
@@ -1,33 +1,25 @@
+<?php 
+$c = new CandidePage('services');
+$c1 = new CandideCollection('services');
+?>
 <h1>services de eosécurité</h1>
 <main class="servicesContainer comingFade">
     <section class="servicesContainer__servicesSection">
         <div class="servicesContainer__servicesSection__cattop">
-            <h2>Nos services</h2>
-            <p>EO Sécurité vous propose également des services annexes dans le but de simplifier la gestion de votre entreprise via la création de paquetage, la réalisation d’audits mais également la réalisation de documents et la livraison de nos produits.</p>
+            <h2><?php $c->text('titre_pour_les_services'); ?></h2>
+            <p><?php $c->text('description_des_services'); ?></p>
         </div>
-        <div class="servicesContainer__servicesSection__cat">
-            <h2 class="white">Personnalisation</h2>
-            <p class="white">- Personnalisation de vêtements / sérigraphie / broderie
-               - Réalisation de paquetage individuels sur mesure
-               - Paquetage standard (même contenue pour chaque salarié, seul les tailles changes) ou paquetage 
-               100% personnalisé (choix des produits, quantités et tailles par salariés selon un budget défini et un 
-               catalogue personnalisé pour votre entreprise)</p>
-        </div>
-        <div class="servicesContainer__servicesSection__cat">
-            <h2 class="white">Contrôles et audits</h2>
-            <p class="white">- Contrôle réglementaire de harnais et matériels travaux en hauteur
-               - Contrôle annuelle obligatoire, vérification des numéros de séries, de l’état général du matériel, 
-               - Contrôle réglementaire des échelles et échafaudages
-               - Audits entreprises ou chantiers</p>
-        </div>
-        <div class="servicesContainer__servicesSection__cat">
-            <h2 class="white">Documents</h2>
-            <p class="white">- Réalisation de documents uniques (création, misle à jour ou aide à la rédaction)</p>
-        </div>
-        <div class="servicesContainer__servicesSection__cat">
-            <h2 class="white">Livraison</h2>
-            <p class="white">Livraison par nos soins (Reims, Epernay, et environs) ou service de livraison via un transporteur</p>
-        </div>
+
+        <?php 
+        foreach(array_reverse($c1->avalaibleItemIds()) as $id){
+            ?>
+            <div class="servicesContainer__servicesSection__cat">
+                <h2 class="white"><?php $c1->text('titre_du_service', $id); ?></h2>
+                <p class="white"><?php $c1->text('description_du_service', $id); ?></p>
+            </div>
+            <?php
+        }
+        ?> 
         <div class="primaryButton servicesContainer__servicesSection__button">
             <button class="primaryButton contactContainer__section__rightdiv__button">Prendre contact avec EO Sécurité</button>
         </div>

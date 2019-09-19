351ecab7b66add2f8aa6c1151f685a6987568e2e
Romain Penchenat <romain.penchenat@gmail.com>
2019-09-19 21:42:45 +0200

    Ajout des partenaires modifiables via candide

--- pages/produits.php
+++ pages/produits.php
@@ -1,6 +1,7 @@
 <?php
 $c[0] = new CandidePage('produits_et_partenaires');
 $c[1] = new CandideCollection('produits');
+$c[2] = new CandideCollection('partenaires');
 ?>
 <main class="comingFade part__container">
     <h1>Les partenaires et les produits proposés par EO Sécurité, Reims</h1>
@@ -29,34 +30,15 @@ $c[1] = new CandideCollection('produits');
     <section class="part__partners">
         <h2>Nos partenaires</h2>
         <div class="part__squareCont">
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/Asset 1-page-001.jpg" alt="Atg"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/aimont.jpg" alt="Aimont"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/jallatte.png" alt="Jallatte"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/CleanSpace_logo_positive_mono.png" alt="Clean Space"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/upower.png" alt="Upower"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/carhartt.png" alt="Carhartt"></div>
-        </div>
-        <div class="part__squareCont">
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/uvex.png" alt="Uvex"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/heckel.jpg" alt="Heckel"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/cepovett.png" alt="Cepovett"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/honeywell.png" alt="Honeywell"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/guyard.jpg" alt="Guyard"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/Etxecom logo.png" alt="Extecom"></div>
-        </div>
-        <div class="part__squareCont">
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/texet.png" alt="Texet"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/engel.png" alt="Engel" width="130px"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/msa.png" alt="Msa"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/gvs-logo.jpg" alt="Gvs"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/INNOTECH.png" alt="Innotech"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/JSP.jpg" alt="Jsp"></div>
-        </div>
-        <div class="part__squareCont">
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/P2L.png" alt="P2L"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/PETZL.png" alt="PETZL"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/Logo JLF.png" alt="Jlf"></div>
-            <div class="part__partnersSquare"> <img src="assets/images/partenaires/defibrion.png" alt="Defibrion"></div>
+            <?php
+            foreach ($c[2]->avalaibleItemIds() as $id) {
+                ?>
+                <div class="part__partnersSquare">
+                    <img src="<?php $c[2]->image("logo",$id,[400,400]) ?>" alt="<?php $c[2]->text("nom",$id) ?>">
+                </div>
+                <?php
+            }
+            ?>
         </div>
     </section>
     <section class="part__contact">
